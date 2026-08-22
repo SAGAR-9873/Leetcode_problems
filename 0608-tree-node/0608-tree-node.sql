9b79399d9d8708a3fe13977d092ffb65f76365b8
SAGAR <godsagar807@gmail.com>
2026-08-22 15:15:50 +0530

    Time: 669 ms (12.87%), Space: 0B (100%) - LeetHub

--- 0608-tree-node/0608-tree-node.sql
+++ 0608-tree-node/0608-tree-node.sql
@@ -5,7 +5,7 @@ on t1.id = t2.p_id)
 SELECT distinct id1 as id ,
 CASE 
      when p_id1 is null then "Root" 
-     when id2 and p_id2 is not NULL then "Inner" 
+     WHEN id2 IS NOT NULL AND p_id2 IS NOT NULL THEN "Inner"
      when id2 is null and p_id2 is null then "Leaf"
      else " "
      end as type
