66a8bb9c4aae6252d5000715ee0c7bbae2f0d7ca
SAGAR <godsagar807@gmail.com>
2026-08-22 18:34:34 +0530

    Time: 1257 ms (76.29%), Space: 0B (100%) - LeetHub

---
+++ 1158-market-analysis-i/1158-market-analysis-i.sql
@@ -0,0 +1,5 @@
+SELECT u.user_id as buyer_id, u.join_date , count(o.buyer_id) as orders_in_2019 
+FROM Users u left join Orders o 
+ON u.user_id = o.buyer_id
+and extract(year from o.order_date) = 2019
+group by u.user_id ;
