09da15e0bd068c1651fdee87a3e42d16c7f9f092
SAGAR <godsagar807@gmail.com>
2026-08-22 13:49:28 +0530

    Time: 582 ms (74.84%), Space: 0B (100%) - LeetHub

--- 0585-investments-in-2016/0585-investments-in-2016.sql
+++ 0585-investments-in-2016/0585-investments-in-2016.sql
@@ -2,8 +2,8 @@ with cte as (
 SELECT pid,tiv_2015,tiv_2016,
 COUNT(Pid) over(partition by tiv_2015) as v,
 COUNT(Pid) over(partition by lat,lon ) as loc
-from Insurance )
+from Insurance)
 select round(sum(tiv_2016),2) as tiv_2016
 from cte 
-where v >= 2 and loc = 1
+where v >= 2 and loc = 1;
 
