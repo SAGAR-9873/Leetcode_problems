c5102a08624cf57c1c7e9484d56a07548185baf0
SAGAR <godsagar807@gmail.com>
2026-08-23 19:52:33 +0530

    Time: 635 ms (54.29%), Space: 0B (100%) - LeetHub

---
+++ 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
@@ -0,0 +1,24 @@
+SELECT 
+        DATE_FORMAT(trans_date, '%Y-%m') as month,
+        country,count(amount) as trans_count,
+        SUM(CASE WHEN state = 'approved' THEN 1 ELSE 0 END) AS approved_count,
+        sum(amount) as trans_total_amount,
+        SUM(CASE WHEN state = 'approved' THEN amount ELSE 0 END) AS approved_total_amount
+FROM Transactions 
+group by country,month
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
