84c62b3e04b9e0e1d3da6e1cb67fda06fad073cd
SAGAR <godsagar807@gmail.com>
2026-08-23 19:54:37 +0530

    Time: 620 ms (62.46%), Space: 0B (100%) - LeetHub

--- 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
+++ 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
@@ -5,7 +5,7 @@ SELECT
         sum(amount) as trans_total_amount,
         SUM(CASE WHEN state = 'approved' THEN amount ELSE 0 END) AS approved_total_amount
 FROM Transactions 
-group by country,month
+group by country,month;
 
 
 
