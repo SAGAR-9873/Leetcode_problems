31beb2b905d8704dc01bb613ef7467e617170632
SAGAR <godsagar807@gmail.com>
2026-08-24 17:26:24 +0530

    Time: 1473 ms (51.09%), Space: 0B (100%) - LeetHub

--- 1341-movie-rating/1341-movie-rating.sql
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -1,17 +1,3 @@
--- with cte1 as (select  u.name ,count(*) as counts 
--- from MovieRating mr left join Users u 
--- on mr.user_id = u.user_id
--- group by u.user_id
--- order by counts desc ,u.name asc
--- limit 1 ),cte2 as 
--- (select m.title , avg(mr.rating) as avg_rating
--- from MovieRating mr left join Movies m
--- on mr.movie_id = m.movie_id
--- where created_at >= "2020-02-01" and created_at <= "2020-02-29"
--- group by m.movie_id
--- order by  avg_rating desc , m.title asc
--- limit 1)
-
 SELECT name as results
 FROM (select  u.name ,count(*) as counts 
 from MovieRating mr left join Users u 
