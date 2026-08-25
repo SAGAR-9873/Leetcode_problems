c9c449e2c4ca28598bfa50014a173278abb62e4f
SAGAR <godsagar807@gmail.com>
2026-08-25 12:28:25 +0530

    Time: 288 ms (66.71%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,10 +1,18 @@
-WITH cte AS (
-    SELECT
-        salary,
-        DENSE_RANK() OVER (ORDER BY salary DESC) AS rnk
-    FROM Employee
-)
-SELECT MAX(salary) AS SecondHighestSalary
-FROM cte
-WHERE rnk = 2;
- 
+-- select max(e1.salary) as SecondHighestSalary
+-- from Employee e1 inner join Employee e2 on
+-- e1.salary < e2.salary
+
+-- select max(salary) as SecondHighestSalary
+-- from Employee
+-- where salary
+-- not in 
+-- (select max(salary) from Employee);
+
+
+SELECT (
+    SELECT DISTINCT salary 
+    FROM Employee 
+    ORDER BY salary DESC 
+    LIMIT 1 OFFSET 1
+) AS SecondHighestSalary;
+
