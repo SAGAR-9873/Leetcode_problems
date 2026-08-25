952e76041cc72d350b62a49b6b6ce53ebe6c1ded
SAGAR <godsagar807@gmail.com>
2026-08-25 12:42:22 +0530

    Time: 485 ms (54.29%), Space: 0B (100%) - LeetHub

--- 0177-nth-highest-salary/0177-nth-highest-salary.sql
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -1,15 +1,15 @@
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
   RETURN (
-    SELECT salary
-    FROM 
-    (SELECT 
-      salary ,
-      DENSE_RANK() OVER (ORDER BY salary DESC) AS SALARY_RANK
+       
+    SELECT DISTINCT salary
+    FROM (
+      SELECT salary, DENSE_RANK() OVER (ORDER BY salary DESC) as rnk
       FROM Employee
-      order by salary  ) AS SALARY_ANALYSIS
-      where SALARY_RANK = N 
-      LIMIT 1
-      
-      );
+    ) t
+    WHERE rnk = N
+  
+  );
 END
+
+   
