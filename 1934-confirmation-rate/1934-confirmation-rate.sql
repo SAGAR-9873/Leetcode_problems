4741dd9a15726f79ebefc368aa336618413b9993
SAGAR <godsagar807@gmail.com>
2026-08-27 19:47:09 +0530

    Time: 808 ms (32.31%), Space: 0B (100%) - LeetHub

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -1,11 +1,3 @@
--- with cte as (select s.user_id ,  sum(case when c.action = "confirmed" then 1
--- when c.action is null then 0 else 0 end) as r,count(c.user_id) as t
--- from Signups s left join Confirmations c 
--- on s.user_id = c.user_id
--- group by s.user_id)
--- select user_id , case when t > 0 then round(r/t,2) else 0 end as confirmation_rate
--- from cte 
-
 SELECT
     s.user_id,
     ROUND(
