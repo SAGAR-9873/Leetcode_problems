bd8058cfaa0c92e361f92d86818c4c5a4e81a477
SAGAR <godsagar807@gmail.com>
2026-08-27 19:30:26 +0530

    Time: 805 ms (32.94%), Space: 0B (100%) - LeetHub

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -1,8 +1,18 @@
-with cte as (select s.user_id ,  sum(case when c.action = "confirmed" then 1
-when c.action is null then 0 else 0 end) as r,count(c.user_id) as t
-from Signups s left join Confirmations c 
-on s.user_id = c.user_id
-group by s.user_id)
-select user_id , case when t > 0 then round(r/t,2) else 0 end as confirmation_rate
-from cte 
+-- with cte as (select s.user_id ,  sum(case when c.action = "confirmed" then 1
+-- when c.action is null then 0 else 0 end) as r,count(c.user_id) as t
+-- from Signups s left join Confirmations c 
+-- on s.user_id = c.user_id
+-- group by s.user_id)
+-- select user_id , case when t > 0 then round(r/t,2) else 0 end as confirmation_rate
+-- from cte 
 
+SELECT
+    s.user_id,
+    ROUND(
+        coalesce (AVG(c.action = 'confirmed'),0),
+        2
+    ) AS confirmation_rate
+FROM Signups s
+LEFT JOIN Confirmations c
+    ON s.user_id = c.user_id
+GROUP BY s.user_id;
